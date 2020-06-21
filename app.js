7b399ba2d515c5f38b38f2eed6b3ac99d3dd18ef
gclou <clo15008@byui.edu>
2020-06-20 22:48:44 -0600

    big commit

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const http = require('http');
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const server = http.createServer(app);
 
-const server = http.createServer();
+app.use(express.static(path.join(__dirname, 'public')))
+   .set('views', path.join(__dirname, 'views'))
+   .set('view engine', 'ejs')
+   .use(bodyParser({extended: false}))
+   .get('/', (req, res, next) => {
+      res.render('pages/template.ejs', {title: 'Welcome to my CSE341 repo', path: '/'});
+    })
+    .use((req, res, next) => {
+        // 404 page
+        res.render('pages/404', {title: '404 - Page Not Found', path: req.url})
+    })
 
 server.listen(5000);
